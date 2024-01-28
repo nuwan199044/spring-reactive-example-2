9fe48112427d86bd577b909c69e2d4a518fe8e19
Nuwan <nuwan199044@gmail.com>
2024-01-28 14:15:14 +0530

    Error handling in reactive programming.

---
+++ src/main/java/com/myapp/springreactiveexample2/exception/ProductRetriveException.java
@@ -0,0 +1,4 @@
+package com.myapp.springreactiveexample2.exception;
+
+public class ProductRetriveException extends RuntimeException {
+}
--- src/main/java/com/myapp/springreactiveexample2/proxy/ProductProxy.java
+++ src/main/java/com/myapp/springreactiveexample2/proxy/ProductProxy.java
@@ -1,9 +1,11 @@
 package com.myapp.springreactiveexample2.proxy;
 
 import com.myapp.springreactiveexample2.entity.Product;
+import com.myapp.springreactiveexample2.exception.ProductRetriveException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 import reactor.core.publisher.Flux;
 
 @Component
@@ -13,8 +15,24 @@ public class ProductProxy {
     private final WebClient webClient;
 
     public Flux<Product> getAll() {
+        var product = new Product(3, "Default");
+
         return webClient.get().uri("/products")
-                .exchangeToFlux(res -> res.bodyToFlux(Product.class));
+                .exchangeToFlux(res -> res.bodyToFlux(Product.class))
+//                .onErrorResume(e -> Flux.just(product));
+//                .onErrorResume(WebClientRequestException.class, e -> Flux.just(product));
+//                .onErrorResume(e -> e.getMessage() != null, e -> Flux.just(product));
+//                .onErrorReturn(product);
+//                .onErrorReturn(WebClientRequestException.class, product);
+//                .onErrorReturn(e -> e.getMessage() != null, product);
+//                .onErrorMap(e -> new ProductRetriveException());
+//                .doOnNext(e -> {
+//                    if (e.getName() == null) throw new RuntimeException();
+//                })
+//                .onErrorContinue((exception, object) -> System.out.println(exception.getMessage()));
+//                .onErrorContinue(RuntimeException.class, (exception, object) -> System.out.println(exception.getMessage()));
+//                .onErrorContinue(e -> e.getMessage() != null, (exception, object) -> System.out.println(exception.getMessage()));
+                .retry(1);
     }
 
 }
