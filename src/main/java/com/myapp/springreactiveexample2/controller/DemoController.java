d26bca9b2253ccb0135446548e4fa278d05b4713
Nuwan <nuwan199044@gmail.com>
2024-01-29 01:21:17 +0530

    Implementing dynamic value flux.

--- src/main/java/com/myapp/springreactiveexample2/controller/DemoController.java
+++ src/main/java/com/myapp/springreactiveexample2/controller/DemoController.java
@@ -1,18 +1,48 @@
 package com.myapp.springreactiveexample2.controller;
 
-import com.myapp.springreactiveexample2.publisher.DemoPublisher;
-import com.myapp.springreactiveexample2.subscriber.DemoSubscriber;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
-import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class DemoController {
 
     @GetMapping
     public void demo() {
-        DemoPublisher publisher = new DemoPublisher(List.of(1,2,3,4,5,6,7,8,9));
-        publisher.subscribe(new DemoSubscriber());
+        Flux<String> f1 = Flux.create(s -> {
+            for (int i=0; i <10; i++) {
+                s.next(UUID.randomUUID().toString());
+            }
+            s.complete();
+        });
+        f1.log().subscribe(new Subscriber<String>() {
+
+            private Subscription subscription;
+            @Override
+            public void onSubscribe(Subscription subscription) {
+                this.subscription = subscription;
+                this.subscription.request(1);
+            }
+
+            @Override
+            public void onNext(String s) {
+                System.out.println(s);
+                this.subscription.request(1);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
     }
 }
